eb6945dde89d6e0ad8a89decbf69a3f281192a01
sjy0917032 <sea71511@gmail.com>
2021-12-16 15:28:13 +0900

    4-4

--- askcompany/instagram/views.py
+++ askcompany/instagram/views.py
@@ -1,6 +1,6 @@
-from django.views.generic import ListView
-from django.http import request,HttpResponse
-from django.shortcuts import render
+from django.views.generic import ListView,DetailView
+from django.http import request,HttpResponse, Http404
+from django.shortcuts import get_object_or_404,render
 from .models import Post
 
 post_list = ListView.as_view(model=Post)
@@ -18,10 +18,18 @@ post_list = ListView.as_view(model=Post)
 #         'q':  q,
 #     })
 
-def post_detail(request, pk):
-    response = HttpResponse()
-    response.write('hello')
-    return response
+# def post_detail(request, pk) -> HttpResponse:
+#     post = get_object_or_404(Post, pk=pk)
+#     # try:
+#     #     post = Post.objects.get(pk=pk)
+#     # except Post.DoesNotExist:
+#     #     raise Http404
+#     return render(request, 'instagram/post_detail.html',{
+#         'post' : post,
+#     })
+    
+post_detail = DetailView.as_view(model=Post)    
+    
 
 def achives_year(request, year):
     return HttpResponse(f"{year}년 archives")
